24923662cc70d9bb94c8968368236bddd0dfb459
redcat243 <henri.peter.noval@gmail.com>
2026-05-28 12:00:55 -0500

    Add files via upload

---
+++ Casks/catbrowser-15.rb
@@ -0,0 +1,16 @@
+cask "catbrowser" do
+  version "1.0.0.0"
+  sha256 :no_check
+
+  # This is the correct Release Download URL for Homebrew
+  url "https://github.com/redcat243/catbrowser-15/releases/download/1.0.0.0/CatBrowser.zip"
+  
+  # This is your actual repository link
+  homepage "https://github.com/redcat243/catbrowser-15/releases"
+  
+  name "CatBrowser"
+  desc "Lightweight tab-less web browser rvamped for macOS 15"
+  depends_on macos: ">= :sonoma"
+  
+  app "CatBrowser.app"
+end
